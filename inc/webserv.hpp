2309ac04f9e38b9499dbf7a5f191df2f5d1f02f6
hboissel <hboissel@student.42.fr>
2023-09-18 12:10:35 +0200

    all includes added

---
+++ inc/webserv.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hboissel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 12:00:41 by hboissel          #+#    #+#             */
+/*   Updated: 2023/09/18 12:09:09 by hboissel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef WEBSERV_HPP
+# define WEBSERV_HPP
+
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netdb.h>
+# include <errno.h>
+# include <arpa/inet.h>
+# include <sys/select.h>
+# include <poll.h>
+# include <epoll.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <dirent.h>
+
+#endif
