e14c6cc9d2f820e49637a4a6fbff0556b3347eee
hboissel <hboissel@student.42.fr>
2023-09-21 17:58:25 +0200

    clean up

--- inc/webserv.hpp
+++ inc/webserv.hpp
@@ -6,15 +6,21 @@
 /*   By: hboissel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 12:00:41 by hboissel          #+#    #+#             */
-/*   Updated: 2023/09/21 17:41:37 by hboissel         ###   ########.fr       */
+/*   Updated: 2023/09/21 17:47:20 by hboissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef WEBSERV_HPP
 # define WEBSERV_HPP
 
+# include <algorithm>
+# include <cstring>
+# include <vector>
 # include <cerrno>
 # include <iostream>
 # include <csignal>
+
+# include <arpa/inet.h>
+# include <unistd.h>
 //# include "TcpServer.hpp"
 
 int	tcp_server(int port);
