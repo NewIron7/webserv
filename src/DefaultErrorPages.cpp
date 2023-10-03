092fc26e1d8ec04cfb4b1fa75d816e145c712be1
hboissel <hboissel@student.42.fr>
2023-10-03 10:04:00 -0400

    plenty of error codes added

--- src/DefaultErrorPages.cpp
+++ src/DefaultErrorPages.cpp
@@ -6,18 +6,18 @@
 /*   By: hboissel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/01 15:09:16 by hboissel          #+#    #+#             */
-/*   Updated: 2023/10/01 19:43:42 by hboissel         ###   ########.fr       */
+/*   Updated: 2023/10/03 10:02:53 by hboissel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "DefaultErrorPages.hpp"
 
 std::string	DefaultErrorPages::genByCode(std::string name, std::string details)
 {
-	std::string gen = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><meta\
-name=\"viewport\" content=\"width=device-width,initial-s\
-cale=1.0\"><title>";
-	gen += name + "</title><style>body{font-family:Arial,sans-serif;background-\
-color:#f5f5f5;text-align:center;}.container{background-color:#fff;borde\
+	std::string gen = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><met";
+	gen += "a name=\"viewport\" content=\"width=device-width,initial-scale=1.0\">";
+	gen += "<title>";
+	gen += name + "</title><style>body{font-family:Arial,sans-serif;background-";
+	gen += "color:#f5f5f5;text-align:center;}.container{background-color:#fff;borde\
 r-radius:8px;box-shadow:0 4px 8px rgba(0,0,0,.2);padding:20px;margin:0 \
 auto;max-width:400px;}h1{color:#e74c3c;}p{color:#333;font-size:18px;}</\
 style></head><body><div class=\"container\"><h1>";
@@ -27,28 +27,138 @@ style></head><body><div class=\"container\"><h1>";
 
 const std::string	DefaultErrorPages::generate(unsigned int code, std::string details)
 {
-	std::string	page;
+	std::string page;
 
 	switch (code)
 	{
+		//4xx Client Errors
 		case 400:
 			page = genByCode("400 - Bad Request", details);
-			break ;
+			break;
+		case 401:
+			page = genByCode("401 - Unauthorized", details);
+			break;
+		case 402:
+			page = genByCode("402 - Payment Required", details);
+			break;
 		case 403:
 			page = genByCode("403 - Forbidden", details);
-			break ;
+			break;
 		case 404:
 			page = genByCode("404 - Not Found", details);
-			break ;
+			break;
 		case 405:
 			page = genByCode("405 - Method Not Allowed", details);
-			break ;
+			break;
+		case 406:
+			page = genByCode("406 - Not Acceptable", details);
+			break;
+		case 407:
+			page = genByCode("407 - Proxy Authentication Required", details);
+			break;
+		case 408:
+			page = genByCode("408 - Request Timeout", details);
+			break;
+		case 409:
+			page = genByCode("409 - Conflict", details);
+			break;
+		case 410:
+			page = genByCode("410 - Gone", details);
+			break;
 		case 411:
 			page = genByCode("411 - Length Required", details);
-			break ;
+			break;
+		case 412:
+			page = genByCode("412 - Precondition Failed", details);
+			break;
 		case 413:
 			page = genByCode("413 - Payload Too Large", details);
-			break ;
+			break;
+		case 414:
+			page = genByCode("414 - URI Too Long", details);
+			break;
+		case 415:
+			page = genByCode("415 - Unsupported Media Type", details);
+			break;
+		case 416:
+			page = genByCode("416 - Range Not Satisfiable", details);
+			break;
+		case 417:
+			page = genByCode("417 - Expectation Failed", details);
+			break;
+		case 418:
+			page = genByCode("418 - I'm a teapot", details);
+			break;
+		case 421:
+			page = genByCode("421 - Misdirected Request", details);
+			break;
+		case 422:
+			page = genByCode("422 - Unprocessable Entity", details);
+			break;
+		case 423:
+			page = genByCode("423 - Locked", details);
+			break;
+		case 424:
+			page = genByCode("424 - Failed Dependency", details);
+			break;
+		case 425:
+			page = genByCode("425 - Too Early", details);
+			break;
+		case 426:
+			page = genByCode("426 - Upgrade Required", details);
+			break;
+		case 428:
+			page = genByCode("428 - Precondition Required", details);
+			break;
+		case 429:
+			page = genByCode("429 - Too Many Requests", details);
+			break;
+		case 431:
+			page = genByCode("431 - Request Header Fields Too Large", details);
+			break;
+		case 451:
+			page = genByCode("451 - Unavailable For Legal Reasons", details);
+			break;
+
+		// 5xx Server Errors
+		case 500:
+			page = genByCode("500 - Internal Server Error", details);
+			break;
+		case 501:
+			page = genByCode("501 - Not Implemented", details);
+			break;
+		case 502:
+			page = genByCode("502 - Bad Gateway", details);
+			break;
+		case 503:
+			page = genByCode("503 - Service Unavailable", details);
+			break;
+		case 504:
+			page = genByCode("504 - Gateway Timeout", details);
+			break;
+		case 505:
+			page = genByCode("505 - HTTP Version Not Supported", details);
+			break;
+		case 506:
+			page = genByCode("506 - Variant Also Negotiates", details);
+			break;
+		case 507:
+			page = genByCode("507 - Insufficient Storage", details);
+			break;
+		case 508:
+			page = genByCode("508 - Loop Detected", details);
+			break;
+		case 510:
+			page = genByCode("510 - Not Extended", details);
+			break;
+		case 511:
+			page = genByCode("511 - Network Authentication Required", details);
+			break;
+
+		default:
+			page = genByCode("500 - Internal Server Error", "An unexpected error occurred.");
+			break;
 	}
+
 	return (page);
 }
